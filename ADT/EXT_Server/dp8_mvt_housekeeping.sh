18e7ac18fdf07624e5fb70901b473e586c2e0ad4
Ian Relf <ian.relf@gmail.com>
2018-06-18 16:31:53 +0100

    On branch YEL-194 Changes to be committed: 	modified:   dp8_mvt_housekeeping.sh 	modified:   housekeeping_mvt.py Now pass the hostname of the AWS RDS instance running the MVT Postgres DB.

--- ADT/EXT_Server/dp8_mvt_housekeeping.sh
+++ ADT/EXT_Server/dp8_mvt_housekeeping.sh
@@ -2,5 +2,5 @@
 
 cd /ADT/scripts
 
-/ADT/scripts/housekeeping_mvt.py -d mvt -u $MVT_SCHEMA_SSM_USERNAME -p $MVT_SCHEMA_SSM_PASSWORD -r $1
+/ADT/scripts/housekeeping_mvt.py -H $RDS_POSTGRES_DATA_INGEST_HOST_NAME -d mvt -u $MVT_SCHEMA_SSM_USERNAME -p $MVT_SCHEMA_SSM_PASSWORD -r $1
 
--- ADT/EXT_Server/housekeeping_mvt.py
+++ ADT/EXT_Server/housekeeping_mvt.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
 # Perform housekeeping on mvt database
-# Version 1.0
-# Phil Hibbs
+# Version 1.1
+# Ian Relf
 
 from datetime import datetime
 import re
@@ -32,11 +32,12 @@ _sql={
 
 def main():
 	parser = argparse.ArgumentParser(description='Perform mvt database housekeeping deletion')
-	parser.add_argument('-d','--database', required=True, help='database name')
-	parser.add_argument('-u','--username', required=True, help='database username')
-	parser.add_argument('-p','--password', nargs='?', default='', help='database password for username')
-	parser.add_argument('-r','--retain', help='oldest date to retain')
-	parser.add_argument('-D','--DEBUG',  default=False, action='store_true', help='Debug mode logging')
+        parser.add_argument('-H','--host',     required=False,                     help='host name')
+        parser.add_argument('-d','--database', required=True,                      help='database name')
+        parser.add_argument('-u','--username', required=True,                      help='database username')
+        parser.add_argument('-p','--password', nargs='?', default='',              help='database password for username')
+        parser.add_argument('-r','--retain',   required=True,                      help='oldest date to retain')
+        parser.add_argument('-D','--DEBUG',    default=False, action='store_true', help='Debug mode logging')
 
 	args = parser.parse_args()
 
@@ -57,10 +58,14 @@ def main():
 			level=logging.INFO
 		)
 
-	dbh=psycopg2.connect(database= args.database, user=args.username, password=args.password)
+        logger=logging.getLogger()
 
-	logger=logging.getLogger()
-	
+        logger.debug("Connecting to MVT DB...")
+
+        dbh=psycopg2.connect(host=args.host, database= args.database, user=args.username, password=args.password)
+
+        logger.debug("Connected to MVT DB.")
+
 	logger.info("mvt housekeeping %s", args.retain)
 
 	logger.info("hk_acl_csv")
